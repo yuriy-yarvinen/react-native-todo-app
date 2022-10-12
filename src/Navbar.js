d3d75c3def1ad3967dacca6239dbf1613e5e397c
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2022-10-12 22:10:01 +0300

    start navbar

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+
+export const Navbar = (props) => {
+	return (
+		<View style={styles.navbar}>
+			<Text style={styles.text}>Todo App</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	navbar: {},
+	text: {},
+});
